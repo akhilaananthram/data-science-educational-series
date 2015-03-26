ad6e0f36acacb5f530b05c8b7d8607671189b189
Akhila Ananthram <asa225@cornell.edu>
2015-03-26 01:07:42 -0400

    formatting

--- spam_detection/spam_detection.py
+++ spam_detection/spam_detection.py
@@ -12,25 +12,28 @@ from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import KFold
 
+
 def parse_args():
-    parser = argparse.ArgumentParser(description="Recommendation Engine Example")
+    parser = argparse.ArgumentParser(description="Recommendation Engine")
     parser.add_argument("--path", dest="path", required=True, type=str,
         help="Path to file. REQUIRED")
     parser.add_argument("--lowercase", dest="lower", action="store_true",
         help="Toggle use of lowercase")
-    parser.add_argument("--classifier", dest="classifier", default="multinomial",
-        choices=["multinomial", "bernoulli", "gaussian", "svm", "logistic"],
-        help="Type of classifier to use")
+    parser.add_argument("--classifier", dest="classifier",
+        default="multinomial", choices=["multinomial", "bernoulli", "gaussian",
+        "svm", "logistic"], help="Type of classifier to use")
 
     return parser.parse_args()
 
+
 def read_data(filename):
-    df = pd.read_csv(filename,sep='\t',header=None)
+    df = pd.read_csv(filename, sep='\t', header=None)
     tags = list(df[0])
     instances = list(df[1])
     instances = [i.decode('utf8') for i in instances]
 
     return np.array(instances), np.array(tags)
+
 
 def tokenize(instances, lowercase=False):
     if lowercase:
@@ -40,7 +43,8 @@ def tokenize(instances, lowercase=False):
 
     return tokens
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     args = parse_args()
 
     print "Reading in Data..."
@@ -64,7 +68,7 @@ if __name__=="__main__":
             for w in i.keys():
                 vocab.add(w)
         vocab = list(vocab)
-        vocab_to_index = {w:i for i,w in enumerate(vocab)}
+        vocab_to_index = {w: i for i, w in enumerate(vocab)}
 
         print "Generating Count Matrix..."
         train_vector = np.zeros((len(train_x), len(vocab)))
@@ -72,7 +76,7 @@ if __name__=="__main__":
             f = train_x[i]
             for w, c in f.iteritems():
                 j = vocab_to_index[w]
-                train_vector[i,j] = c
+                train_vector[i, j] = c
 
         test_vector = np.zeros((len(test_x), len(vocab)))
         for i in xrange(len(test_x)):
@@ -81,7 +85,7 @@ if __name__=="__main__":
                 # Do not count words that were not in the training set
                 if w in vocab_to_index:
                     j = vocab_to_index[w]
-                    test_vector[i,j] = c
+                    test_vector[i, j] = c
 
         print "Training TFIDF..."
         tfidf = TfidfTransformer()
@@ -89,15 +93,15 @@ if __name__=="__main__":
         test_tfidf = tfidf.transform(test_vector)
 
         print "Training Classifier..."
-        if args.classifier=="multinomial":
+        if args.classifier == "multinomial":
             classifier = MultinomialNB()
-        elif args.classifier=="bernoulli":
+        elif args.classifier == "bernoulli":
             classifier = BernoulliNB()
-        elif args.classifier=="gaussian":
+        elif args.classifier == "gaussian":
             classifier = GaussianNB()
-        elif args.classifier=="svm":
+        elif args.classifier == "svm":
             classifier = SVC()
-        elif args.classifier=="logistic":
+        elif args.classifier == "logistic":
             classifier = LogisticRegression()
 
         classifier.fit(train_tfidf.toarray(), train_y)
